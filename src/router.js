e4b1932d547114a4929867c0f7e8d660af776a25
Kiran <kiranbangar1216@gmail.com>
2019-09-04 12:00:53 +0530

    .gitignore file

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
 
 Vue.use(Router);
 
@@ -13,13 +19,13 @@ export default new Router({
       name: 'home',
       component: Home,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
   ],
 });
